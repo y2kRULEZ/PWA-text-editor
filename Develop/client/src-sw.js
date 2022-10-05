27fd55cdf71ed968deef93f06200775378641721
y2kRULEZ <imanpace7@gmail.com>
2022-10-05 10:55:13 -0400

    Inject manifest,plugins,css loader

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,7 +26,15 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+
 registerRoute(
-  ({})
+  ({request})=>['style','script','worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName:'asset-cache',
+    plugins:[
+      new CacheableResponsePlugin({
+        statuses:[0,200],
+      })
+    ]
+  })
 );
